3f81ad030e1f6b136dc52a1023e800d2f83d075e
Nanami Kimoto <46579968+nakimoto@users.noreply.github.com>
2021-02-08 20:08:39 -1000

    Update ziplist.js
    Fixed my for loop

--- ziplist.js
+++ ziplist.js
@@ -3,7 +3,7 @@ const test2 = [1, 2, 3];
 
 function zipList(list1, list2) {
   const result = [];
-  for (let i = 0 < list1.length; i++) {
+  for (let i = 0; i < list1.length; i++) {
     result.push(list1[i], list2[i]);
   }
   return result;
